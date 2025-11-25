c01718ceea218e553b965f60dda646e5a7222261
prinits1998 <meniyapriti@gmail.com>
2025-11-25 12:32:29 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const express = require('express');
+
+http.createServer((req , resp)=>{
+    resp.write("<p>hello i am meniya priti</p>");
+    respend("hello");
+}).listen(4800);
